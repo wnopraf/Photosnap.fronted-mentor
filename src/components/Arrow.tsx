dfbdf7d80b5c53fbf9501815d8def921079d5a8b
Rafa Prieto <rapribu@gmail.com>
2021-01-11 12:34:46 +0100

    Add Arrow component

---
+++ src/components/Arrow.tsx
@@ -0,0 +1,23 @@
+export const Arrow = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="80.695"
+    height="33.392"
+    viewBox="0 0 80.695 33.392"
+  >
+    <defs>
+      <style>
+        {'.a,.b{stroke:white;stroke-linecap:round;}.b{stroke-linejoin:round;}'}
+      </style>
+    </defs>
+    <g transform="translate(-376 -479.303)">
+      <path className="a" d="M0,0H80" transform="translate(376 496)" />
+      <path
+        className="b"
+        d="M11,16,5.675,8,0,0"
+        transform="translate(445 480)"
+      />
+      <line className="a" x1="11" y2="16" transform="translate(445 496)" />
+    </g>
+  </svg>
+)
