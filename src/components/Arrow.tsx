06b34e4ba744034da1f09f800e5fdc7cfe46db9d
Rafa Prieto <rapribu@gmail.com>
2021-01-15 18:54:06 +0100

    Replace fixed size length by dinamic ones

--- src/components/Arrow.tsx
+++ src/components/Arrow.tsx
@@ -1,9 +1,10 @@
 export const Arrow = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="80.695"
-    height="33.392"
+    width="1em"
+    height="1em"
     viewBox="0 0 80.695 33.392"
+    className="arrow"
   >
     <defs>
       <style>
