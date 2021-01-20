b15a5e178250b6d9780e0521bfc4691f0e6799b8
Rafa Prieto <rapribu@gmail.com>
2021-01-20 16:55:16 +0100

    Replace named import by default import

--- src/__fixtures__/Page.tsx
+++ src/__fixtures__/Page.tsx
@@ -1,4 +1,4 @@
-import { Footer } from '../components/Footer'
+import Footer from '../components/Footer'
 import { Layout } from '../components/Grid'
 import { Header } from '../components/Header'
 
