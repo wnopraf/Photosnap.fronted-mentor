be4c25bb43ae2614fe4957402679fe02789a7a32
Rafa Prieto <rapribu@gmail.com>
2021-01-20 12:02:34 +0100

    Add complete page component fixture

---
+++ src/__fixtures__/Page.tsx
@@ -0,0 +1,11 @@
+import { Footer } from '../components/Footer'
+import { Layout } from '../components/Grid'
+import { Header } from '../components/Header'
+
+export default () => (
+  <>
+    <Header />
+    <Layout />
+    <Footer />
+  </>
+)
